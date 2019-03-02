b54cb31d5ae57d6d9c0b6dcc473db0dc20e9a34d
kamiljot <kamiljotzia@gmail.com>
2019-03-02 17:53:27 +0200

    projekt

--- main.cpp
+++ main.cpp
@@ -2,6 +2,34 @@
 using namespace std;
 //Compiler version g++ 6.3.0
 
+struct result{
+    int res, rem;
+};
+
+result calc(int a, int b, int sign){
+    result result1{};
+
+    switch (sign){
+        case 43:
+            result1.res = a + b;
+            result1.rem = 0;
+            break;
+        case 45:
+            result1.res = a - b;
+            result1.rem = 0;
+            break;
+        case 42:
+            result1.res = a * b;
+            result1.rem = 0;
+            break;
+        case 47:
+            result1.res = a / b;
+            result1.rem = a % b;
+            break;
+    }
+    return result1;
+}
+
 int string_lenght(char roman[]){
     int l = 0;
     while (roman[l] != '\0')
@@ -9,39 +37,75 @@ int string_lenght(char roman[]){
     return l;
 }
 
-int roman_to_arabic(int ascii){
+int roman_to_arabic(int ascii[], int lenght){
+    int * ascii_str;
+    ascii_str = new int[lenght];
+    int a = 0;
 
-        switch(ascii){
+    for (int i = 0; i < lenght; i++) {
+        switch (ascii[i]) {
             case 73:
-                return 1;
-
+                ascii_str[i] = 1;
+                break;
             case 86:
-                return 5;
-
+                ascii_str[i] = 5;
+                break;
             case 88:
-                return 10;
-
+                ascii_str[i] = 10;
+                break;
             case 76:
-                return 50;
-
+                ascii_str[i] = 50;
+                break;
             case 67:
-                return 100;
-
+                ascii_str[i] = 100;
+                break;
             case 68:
-                return 500;
-
+                ascii_str[i] = 500;
+                break;
             case 77:
-                return 1000;
-
+                ascii_str[i] = 1000;
+                break;
         }
+        cout << ascii_str[i] << endl;
+    }
+    if (lenght == 1){
+        a = ascii_str[0];
+    }
+    else{
+        if (lenght == 2){
+            if (ascii_str[0] >= ascii_str[1]){
+                a = ascii_str[0] + ascii_str[1];
+            }
+            else {
+                a = ascii_str[1] - ascii_str[0];
+            }
+        }
+        else{
+            if(ascii_str[0] >= ascii_str[1]){
+                a = ascii_str[0];
+            }
+            for (int i = lenght - 1; i > 0; i--){
+                if (ascii_str[i-1] >= ascii_str[i]){
+                    a += ascii_str[i];
+                }
+                else{
+                    a += ascii_str[i] - ascii_str[i-1];
+                    i--;
+                }
+            }
+        }
+    }
+    delete [] ascii_str;
+    return a;
 }
 
 int main()
 {
 
     char roman_a[16], roman_b[16];
-    int lenght_a, lenght_b, ascii_a[16], ascii_b[16], arabic_a[16], arabic_b[16];
+    int lenght_a, lenght_b, ascii_a[16], ascii_b[16], arabic_str_a[16], arabic_str_b[16], arabic_a = 0, arabic_b = 0;
     int sign = getchar();
+    result result1{};
 
     cin >> roman_a >> roman_b;
 
@@ -58,16 +122,29 @@ int main()
         //test */         cout << i << " " << ascii_b[i] << endl;
     }
 
+    arabic_a = roman_to_arabic(ascii_a, lenght_a);
+    arabic_b = roman_to_arabic(ascii_b, lenght_b);
 
-    for (int i = 0; i < lenght_a; i++){
-        arabic_a[i] = roman_to_arabic(ascii_a[i]);
-        //test */        cout << i << " " << arabic_a[i] << endl;
-    }
+    cout << arabic_a << endl << arabic_b << endl;
 
-    for (int i = 0; i < lenght_b; i++){
-        arabic_b[i] = roman_to_arabic(ascii_b[i]);
-        //test */         cout << i << " " << arabic_b[i] << endl;
+
+    result1 = calc(arabic_a, arabic_b, sign);
+
+    if (result1.rem == 0){
+        cout << result1.res << endl;
     }
+    else{
+        cout << result1.res << " " << result1.rem << endl;
+    }
+//    for (int i = 0; i < lenght_a; i++){
+//        arabic_str_a[i] = roman_to_arabic(ascii_a[i]);
+        //test */        cout << i << " " << arabic_str_a[i] << endl;
+//    }
+
+//    for (int i = 0; i < lenght_b; i++){
+//        arabic_str_b[i] = roman_to_arabic(ascii_b[i]);
+        //test */         cout << i << " " << arabic_str_b[i] << endl;
+//    }
 
 
     //test */      cout << lenght_a << " " << lenght_b << endl;
